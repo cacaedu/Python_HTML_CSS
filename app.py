85544c982e1ffecd7ff12704bff2d0a2f2ce5aa4
cacaedu <cacaedu51@gmail.com>
2021-08-21 18:05:17 +0000

    Blog

--- app.py
+++ app.py
@@ -5,16 +5,26 @@ from flask import Flask, request, session, g, redirect, abort, render_template, 
 DATABASE = "blog.db"
 SECRET_KEY = 'pudim'
 
-
 app = Flask(__name__) # faz tudo: cria o servidor
 app.config.from_object(__name__)
 
 def conectar_bd():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/")
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
 
+@app.teardown_request
+def depois_request(exc):
+    g.bd.close()
+
+@app.route("/")
+def exibir_entradas():
+    return render_template("exibir_entradas.html") # NÃO PRECISA INDICAR A PASTA?????
+
+@app.route("/hello")
 def pagina_inicial():
-    return "<input autofocus></input>"
+    return "<h1 style='color: red'>Hello</h1>"
     
 
