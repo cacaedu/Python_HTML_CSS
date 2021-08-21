9218f8cf87e53a53e7aa4c6bcb51a89d76b78ece
cacaedu <cacaedu51@gmail.com>
2021-08-21 16:44:50 +0000

    Com banco de dados

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-print("OK")
-print("Olá!")
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort, render_template, flash
+
+# Configuração
+DATABASE = "blog.db"
+SECRET_KEY = 'pudim'
+
+
+app = Flask(__name__) # faz tudo: cria o servidor
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route("/")
+
+def pagina_inicial():
+    return "<input autofocus></input>"
+    
+
